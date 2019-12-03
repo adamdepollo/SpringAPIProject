14338312d17d92bef7fa9e5f50119f9735c7ce18
adamdepollo <adamdepollo@gmail.com>
2019-12-03 13:51:18 -0500

    added coding for api key to HomeController

--- src/main/java/co/grandcircus/SpringAPIProject/controllers/HomeController.java
+++ src/main/java/co/grandcircus/SpringAPIProject/controllers/HomeController.java
@@ -1,11 +1,15 @@
 package co.grandcircus.SpringAPIProject.controllers;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
+	
+	@Value("${ticketmaster.key}")
+	String ticketmasterKey;
 
 	@RequestMapping("/")
 	public ModelAndView homePage() {
